2c08868909779854b4f8a1a6b0d84a13495287c8
jawaharkmea-ops <jawaharkmea@gmail.com>
2026-08-19 20:30:23 +0530

    Add files via upload

---
+++ radius_of_circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int r;
+    float pi,area;
+    printf("Enter the radius of the circle: \n");
+    pi=3.14;
+    scanf("%d",&r);
+    area=pi*r*r;
+    printf("Area=%f",area);
+    return 0;
+
+}
