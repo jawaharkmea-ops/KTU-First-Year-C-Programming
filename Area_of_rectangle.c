5685c24cf873572f473897b95bea5ca2d3de5971
jawaharkmea-ops <jawaharkmea@gmail.com>
2026-08-19 20:36:56 +0530

    Add files via upload

---
+++ Area_of_rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,area;
+    printf("Enter the length of rectangle:\n");
+    scanf("%d",&a);
+    printf("Enter the breath of rectangle:\n");
+    scanf("%d",&b);
+    printf("Area of Recatangle=%d",a*b);
+    return 0;
+}
+
